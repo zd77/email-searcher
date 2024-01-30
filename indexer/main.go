8f0549467b2d90b04ec61a1be209cac89dc375ab
Dereck Zavala <zavaladereck77@gmail.com>
2024-01-30 11:01:29 -0600

    Refactored based on profiling results

--- indexer/main.go
+++ indexer/main.go
@@ -12,6 +12,7 @@ import (
 	"path/filepath"
 	"runtime/pprof"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -46,6 +47,9 @@ type Data struct {
 	Index   string  `json:"index"`
 	Records []Email `json:"records"`
 }
+
+const batchSize = 100
+const maxConcurrentRequests = 10
 
 func extractEmailInfo(lines *bufio.Scanner) Email {
 	email := Content{}
@@ -97,76 +101,134 @@ func extractEmailInfo(lines *bufio.Scanner) Email {
 }
 
 func processDirectory(dirPath string) []Email {
-	emails := []Email{}
+	var (
+		wg     sync.WaitGroup
+		mu     sync.Mutex
+		emails []Email
+	)
+	processEntry := func(entry os.DirEntry) {
+		defer wg.Done()
+		if entry.IsDir() {
+			subDirPath := filepath.Join(dirPath, entry.Name())
+			subEmails := processDirectory(subDirPath)
+			mu.Lock()
+			emails = append(emails, subEmails...)
+			mu.Unlock()
+		} else {
+			email := processEmailFile(filepath.Join(dirPath, entry.Name()))
+			mu.Lock()
+			emails = append(emails, email)
+			mu.Unlock()
+		}
+	}
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
 		fmt.Printf("Error reading directory: %v", dirPath)
 		panic(err)
 	}
 	for _, entry := range entries {
-		if string(entry.Name()[0]) == "." {
+		if strings.HasPrefix(entry.Name(), ".") {
 			continue
 		}
-		if entry.IsDir() {
-			emails = append(emails, processDirectory(filepath.Join(dirPath, entry.Name()))...)
-		} else {
-			email := processEmailFile(filepath.Join(dirPath, entry.Name()))
-			emails = append(emails, email)
-		}
+		wg.Add(1)
+		go processEntry(entry)
 	}
+	wg.Wait()
 	return emails
 }
 
 func processEmailFile(filePath string) Email {
-	file, _ := os.Open(filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %v", filePath, err)
+		panic(err)
+	}
+	defer file.Close()
 	lines := bufio.NewScanner(file)
-	email := extractEmailInfo(lines)
-	return email
+	return extractEmailInfo(lines)
 }
 
-func bulkedIndexData(emails []Email) {
+func bulkedIndexData(emails []Email, uri string) {
 	user := "admin"
 	password := "Complexpass#123"
-	data := Data{
-		Index:   "enron_emails",
-		Records: emails,
-	}
-	jsonData, _ := json.MarshalIndent(data, "", " ")
-	req, err := http.NewRequest("POST", "http://localhost:4080/api/_bulkv2", bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Fatal("Error reading request. ", err)
-	}
-	req.SetBasicAuth(user, password)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+	client := &http.Client{}
+	var wg sync.WaitGroup
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < len(emails); i += batchSize {
+		wg.Add(1)
+		go func(start, end int) {
+			defer wg.Done()
+			if end > len(emails) {
+				end = len(emails)
+			}
+			if start >= end {
+				return
+			}
+			data := Data{
+				Index:   "enron_emails",
+				Records: emails[start:end],
+			}
+			var jsonData bytes.Buffer
+			if err := json.NewEncoder(&jsonData).Encode(data); err != nil {
+				fmt.Printf("Error encoding JSON: %v", err)
+				panic(err)
+			}
+			req, err := http.NewRequest("POST", uri+"/api/_bulkv2", &jsonData)
+			if err != nil {
+				fmt.Printf("Error creating HTTP request: %v ", err)
+				panic(err)
+			}
+			req.SetBasicAuth(user, password)
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Printf("Error making HTTP request: %v\n", err)
+				panic(err)
+			}
+			defer resp.Body.Close()
+		}(i, i+batchSize)
+		if i%maxConcurrentRequests == 0 {
+			wg.Wait()
+		}
 	}
-	defer resp.Body.Close()
+	wg.Wait()
+}
+
+func envSelector(env string) (string, string) {
+	if env != "LOCAL" {
+		return "../../enron_mail_20110402/maildir2", "http://load-balancer-dev-2129612410.us-east-1.elb.amazonaws.com"
+	}
+	return "../../enron_mail_20110402/maildir", "http://localhost:4080"
 }
 
 func main() {
-	cpu, err := os.Create("go.performance")
+	cpu, err := os.Create("cpu.perf")
 	if err != nil {
 		log.Fatal(err)
 	}
 	pprof.StartCPUProfile(cpu)
 	defer pprof.StopCPUProfile()
 
-	startTime := time.Now()
-	var pathFlag string
-	flag.StringVar(&pathFlag, "path", ".", "Directory route")
+	startTimeAll := time.Now()
+	var envFlag string
+	flag.StringVar(&envFlag, "env", "LOCAL", "Directory route")
 	flag.Parse()
-	directory, err := filepath.Abs(pathFlag)
+	path, uri := envSelector(envFlag)
+	fmt.Println("Environment -> ", envFlag)
+	directory, err := filepath.Abs(path)
 	if err != nil {
 		fmt.Println("Error getting the absolute path")
 		return
 	}
-	fmt.Println("->", directory)
+	startTimePD := time.Now()
 	emails := processDirectory(directory)
-	bulkedIndexData(emails)
-	endTime := time.Now()
-	fmt.Printf("emails[%d] - duration[%s]", len(emails), endTime.Sub(startTime))
+	endTimePD := time.Now()
+	fmt.Printf("emails[%d] - duration[%s]\n", len(emails), endTimePD.Sub(startTimePD))
+	startTimeBID := time.Now()
+	bulkedIndexData(emails, uri)
+	endTimeBID := time.Now()
+	fmt.Printf("Sending data to zinc [%s]\n", endTimeBID.Sub(startTimeBID))
+	endTimeAll := time.Now()
+	fmt.Printf("[%s]", endTimeAll.Sub(startTimeAll))
 }
